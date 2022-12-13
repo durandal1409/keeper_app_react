c27679be21bd9424e55180675ea8e6a1c2e3b944
kladovamaria <mackladova@ya.ru>
2022-12-12 21:53:32 -0500

    Updates

---
+++ precache-manifest.df4ad4287253a6841b4e33597984f2b7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5fc479296b5981325d4649baf69cf5d",
+    "url": "/keeper_app_react/index.html"
+  },
+  {
+    "revision": "8f5d874518fd28bb1521",
+    "url": "/keeper_app_react/static/js/2.d18fa6d1.chunk.js"
+  },
+  {
+    "revision": "b7718ecd73df4f00bb20",
+    "url": "/keeper_app_react/static/js/main.ffe5a7ad.chunk.js"
+  },
+  {
+    "revision": "b360b37df094593ca3bd",
+    "url": "/keeper_app_react/static/js/runtime-main.fbc076b1.js"
+  }
+]);
